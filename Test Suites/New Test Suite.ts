0a5a135f887ed2b9358b71df4267e8e8fac27e22
khang.dang <khang.dang@localhost>
2021-12-22 10:03:36 +0700

    First

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>151cced8-8e4e-4412-b558-61d72e348a68</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c8093d6-3e6b-4f40-8e95-0c06450c5988</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case (2)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
